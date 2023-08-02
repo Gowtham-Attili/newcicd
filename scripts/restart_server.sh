9854120ac2765643794f39841336a7998dcc78f5
Gowtham-Attili <118279026+Gowtham-Attili@users.noreply.github.com>
2023-08-02 10:25:34 +0530

    Update restart_server.sh

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Replace 'your-project-name' with the actual name of your Java application.
-sudo systemctl restart your-project-name.service
+sudo systemctl restart mavenjava.service
